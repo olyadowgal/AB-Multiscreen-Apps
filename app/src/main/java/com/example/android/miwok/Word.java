cce0e444102a7ed3f3a0953babf9d5618ab2ba67
DowgalOlya <dowgalolya@gmail.com>
2018-11-05 12:50:52 +0200

    Miwok

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,58 @@
+package com.example.android.miwok;
+
+/**
+ * {@link Word} represents a vocabulary word that the user wants to learn.
+ * It contains a default translation and a Miwok translation for that word.
+ */
+
+
+public class Word {
+    //    Default translation for the word
+    private String mDefaultTranslation;
+
+    //    Miwok translation for the word
+    private String mMiwokTranslation;
+
+    //    Image id for the word
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+    private int mAudioResourceId;
+
+    private static final int NO_IMAGE_PROVIDED = -1;
+
+
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mAudioResourceId = audioResourceId;
+    }
+
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId,int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResourceId;
+    }
+
+
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+    public int getAudioResourceId() {
+        return mAudioResourceId;
+    }
+
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+
+}
